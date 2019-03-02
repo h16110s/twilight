8e819d2c5771ceb54fb49b24428264393a19060b
Hirohito <h_16110s@yahoo.co.jp>
2019-03-02 22:50:15 +0900

    update Client, Server

--- Client/Client.ino
+++ Client/Client.ino
@@ -10,6 +10,7 @@ void ERROR(String message);
 void playMusic(int num);
 bool isBusy();
 void printData(byte *recvData);
+const char* getAddress();
 // ======================================
 
 // PIN ==================================
@@ -33,101 +34,118 @@ DFRobotDFPlayerMini myDFPlayer;
 // ===================================================
 
 void setup() {
-  Serial.begin (9600);
-  myDFSerial.begin (9600);
+    Serial.begin (9600);
+    myDFSerial.begin (9600);
 
-  pinMode(soundBusy,INPUT);
-  pinMode(sw, INPUT);
-  pinMode(dip1, INPUT);
-  pinMode(dip2, INPUT);
-  pinMode(dip3, INPUT);
-  pinMode(dip4, INPUT);
-  pinMode(motorR,OUTPUT);
-  pinMode(motoeL,OUTPUT);
-  pinMode(ledG, OUTPUT);
-  pinMode(ledR, OUTPUT);
+    pinMode(soundBusy,INPUT);
+    pinMode(sw, INPUT);
+    pinMode(dip1, INPUT);
+    pinMode(dip2, INPUT);
+    pinMode(dip3, INPUT);
+    pinMode(dip4, INPUT);
+    pinMode(motorR,OUTPUT);
+    pinMode(motoeL,OUTPUT);
+    pinMode(ledG, OUTPUT);
+    pinMode(ledR, OUTPUT);
 
-  // LED MODE CHANGE (Initialize Status) =============
-  digitalWrite(ledG,HIGH);
-  digitalWrite(ledR,HIGH);
-  // =================================================
+    // LED MODE CHANGE (Initialize Status) =============
+    digitalWrite(ledG,HIGH);
+    digitalWrite(ledR,HIGH);
+    // =================================================
 
-  //DFPlayer Initialize ==============================
-  //set softwareSerial for DFPlayer-mini mp3 module 
-  if(!myDFPlayer.begin(myDFSerial)){
-    ERROR("DFPlayer初期化エラー");
-  }
-  Serial.println(F("DFPlayer 接続済み"));
-  // =================================================
+    //DFPlayer Initialize ==============================
+    //set softwareSerial for DFPlayer-mini mp3 module 
+    if(!myDFPlayer.begin(myDFSerial)){
+        ERROR("DFPlayer初期化エラー");
+    }
+    Serial.println(F("DFPlayer 接続済み"));
+    // =================================================
 
-  // Network Initialize ==============================
-  Mirf.spi = &MirfHardwareSpi;
-  Mirf.init();
-  Mirf.setRADDR((byte *)"clie1"); //とりあえず名前指定で
-    // ４つのDIPスイッチ使ってアドレス別にする予定？
-  Mirf.payload = sizeof(unsigned long);
-  Mirf.config();
-  Serial.println("Listening...");
-  // =================================================
+    // Network Initialize ==============================
+    Mirf.spi = &MirfHardwareSpi;
+    Mirf.init();
+    const char* address = getAddress();
+    Mirf.setRADDR((byte *) address); 
+    Mirf.payload = sizeof(unsigned long);
+    Mirf.config();
+    Serial.println("Listening...");
+    // =================================================
 }
 
 void loop() {
-  // LED MODE CHANGE (Green Status)===================
-  digitalWrite(ledG,HIGH);
-  digitalWrite(ledR,LOW);
-  // =================================================
+    // LED MODE CHANGE (Green Status)===================
+    digitalWrite(ledG,HIGH);
+    digitalWrite(ledR,LOW);
+    // =================================================
 
-  byte recvData[Mirf.payload];  // 最大32bit, byteの行列なので要素数は4
-  Mirf.getData(recvData);
-  // if (recvData[0] == buttonOpen) {  // スイッチを押していない情報であれば
-  //   Serial.println("OFF");
-  //   digitalWrite(ledPin, LOW);
-  // } 
-  // else {                          // スイッチを押した情報であれば
-  //   Serial.println("ON");
-  //   digitalWrite(ledPin, HIGH);
-  // }
-  delay(100);
+    byte recvData[Mirf.payload];  // 最大32bit, byteの行列なので要素数は4
+    Mirf.getData(recvData);
+    // if (recvData[0] == buttonOpen) {  // スイッチを押していない情報であれば
+    //   Serial.println("OFF");
+    //   digitalWrite(ledPin, LOW);
+    // } 
+    // else {                          // スイッチを押した情報であれば
+    //   Serial.println("ON");
+    //   digitalWrite(ledPin, HIGH);
+    // }
+    delay(100);
 }
 
 
 void ERROR(String message){
-  // LED MODE CHANGE (Error Status)===================
-  digitalWrite(ledG,LOW);
-  digitalWrite(ledR,HIGH);
-  //==================================================
-  while(true){
-    Serial.print("Error:");
-    Serial.println(message);
+    // LED MODE CHANGE (Error Status)===================
+    digitalWrite(ledG,LOW);
     digitalWrite(ledR,HIGH);
-    delay(1000);
-    digitalWrite(ledR,LOW);
-    delay(1000);
-  }
+    //==================================================
+    while(true){
+        Serial.print("Error:");
+        Serial.println(message);
+        digitalWrite(ledR,HIGH);
+        delay(1000);
+        digitalWrite(ledR,LOW);
+        delay(1000);
+    }
 }
 
 void playMusic(int num){
-  myDFPlayer.volume(15);
-  myDFPlayer.play(num);
-  delay(10);
-  while(isBusy);
-  myDFPlayer.volume(0);
+    myDFPlayer.volume(15);
+    myDFPlayer.play(num);
+    delay(10);
+    while(isBusy);
+    myDFPlayer.volume(0);
 }
 
 bool isBusy(){
-  if(digitalRead(soundBusy) == HIGH){
-    return true;
-  }
-  else{
-    return false;
-  }
+    if(digitalRead(soundBusy) == HIGH){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
 
 void printData(byte *recvData){
-  Serial.print("recvData: ");
-  for (int i = 0;i < Mirf.payload;i++) {
-    Serial.print(recvData[i]);
-    Serial.print(" ");
-  }
-  Serial.println();
+    Serial.print("recvData: ");
+    for (int i = 0;i < Mirf.payload;i++) {
+        Serial.print(recvData[i]);
+        Serial.print(" ");
+    }
+    Serial.println();
+}
+
+const char* getAddress(){
+    int address = 0;
+    if(digitalRead(dip1) == HIGH){
+        address += 1;
+    }
+    if(digitalRead(dip2) == HIGH){
+        address += 2;
+    }
+    if(digitalRead(dip3) == HIGH){
+        address += 4;
+    }
+    if(digitalRead(dip4) == HIGH){
+        address += 8;
+    }
+    return "client" + address;
 }
--- Server/Server.ino
+++ Server/Server.ino
@@ -1,9 +1,38 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+constexpr uint8_t RST_PIN = 9;          // Configurable, see typical pin layout above
+constexpr uint8_t SS_PIN = 10;         // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+String getUID(){
+    // get RFIC UID =====================================================================
+    String strBuf[mfrc522.uid.size];
+    for (byte i = 0; i < mfrc522.uid.size; i++) {
+        strBuf[i] =  String(mfrc522.uid.uidByte[i], HEX);
+        if(strBuf[i].length() == 1){
+          strBuf[i] = "0" + strBuf[i];
+        }
+    }
+    return strBuf[0] + " " + strBuf[1] + " " + strBuf[2] + " " + strBuf[3];
+    // ==================================================================================
+}
 
 
 void setup(){
-
+    Serial.begin(9600);   // Initialize serial communications with the PC
+    while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+    SPI.begin();      // Init SPI bus
+    mfrc522.PCD_Init();   // Init MFRC522
+    mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+    Serial.println(F("Scan PICC to see UID, SAK, type, and data blocks..."));
 }
 
 void loop(){
+    String strUID = getUID();
     
+    // Dump debug info about the card; PICC_HaltA() is automatically called
+    mfrc522.PICC_DumpToSerial(&(mfrc522.uid));
+    delay(10);
 }
